35e03e4578d43fe9c26c0c79ba95768ff6f4227f
Thiago Marcelo Uchôa Marques <thiago.uchoa18@gmail.com>
2022-05-06 17:27:27 -0400

    video 06 - adding main character animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,7 +7,10 @@ public class Player : MonoBehaviour
 
     public float speed;
     public float jumpForce;
+
     private Rigidbody2D rig;
+    private Animator anim;
+
     private bool onTheGround;
     private bool doubleJump;
 
@@ -15,6 +18,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
         onTheGround = true;
         doubleJump = false;
     }
@@ -22,40 +27,73 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move();   
+        move();
         jump();
     }
 
-    void move(){
+    void move()
+    {
+        // a função 'Input.GetAxis("Horizontal")' varia entre os valores -1, 0 e 1
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * speed;
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            anim.SetBool("walk", true);
+
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                transform.eulerAngles = new Vector3(0f,180f,0f);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            }
+
+        }
+        else
+        {
+            anim.SetBool("walk", false);
+        }
     }
 
-    void jump(){
-        if(Input.GetButtonDown("Jump")){
+    void jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
 
-            if(onTheGround == true){
-                rig.AddForce(new Vector2(0f, jumpForce),ForceMode2D.Impulse);
+            if (onTheGround == true)
+            {
+                rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
                 doubleJump = true;
-            }else{
-                if(doubleJump){
-                    rig.AddForce(new Vector2(0f, jumpForce),ForceMode2D.Impulse);
+                anim.SetBool("jump", true);
+            }
+            else
+            {
+                if (doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
                     doubleJump = false;
                 }
             }
-             
+
 
         }
     }
 
-    void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.layer == 8){
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
             onTheGround = true;
+            anim.SetBool("jump", false);
         }
     }
 
-    void OnCollisionExit2D(Collision2D other) {
-        if(other.gameObject.layer == 8){
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
             onTheGround = false;
         }
     }
